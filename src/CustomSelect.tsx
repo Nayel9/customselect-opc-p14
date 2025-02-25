7c8bbdf09d843f379f9f9984d0c0fa525e1d95b3
Nahuel Vainer <nahuel9@hotmail.fr>
2025-02-25 15:22:58 +0100

    Final Commit

--- src/CustomSelect.tsx
+++ src/CustomSelect.tsx
@@ -8,11 +8,12 @@ interface CustomSelectProps {
     defaultOption: string;
     onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
     value: string;
+    dataTestId: string;
 }
 
-export function CustomSelect({name, id, required, options, defaultOption, onChange, value}: CustomSelectProps) {
+export function CustomSelect({name, id, required, options, defaultOption, onChange, value, dataTestId}: CustomSelectProps) {
     return (
-        <select name={name} id={id} required={required} onChange={onChange} value={value}>
+        <select name={name} id={id} required={required} onChange={onChange} value={value} data-testid={dataTestId}>
             <option value="">{defaultOption}</option>
             {options.map((option) => (
                 <option key={option.value} value={option.value}>
